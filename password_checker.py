e15a34d85a69f3ba33d63f20ce8d65e23027b7c4
Zafarbek Obloqulov <zafarbekobloqulov160001@gmail.com>
2026-03-01 12:20:02 +0500

    Enhance password_Add basic password checker using if statementchecker.py with comments
    Added comments to explain password strength check.

--- password_checker.py
+++ password_checker.py
@@ -1,1 +1,8 @@
-
+#This file check your password
+# This program checks whether a password is strong or weak.
+# if your password more than 9 character,your password is strong
+password=input("your password=")
+if len(password)>=9:
+    print("strong password")
+else :
+    print("weak password")    
