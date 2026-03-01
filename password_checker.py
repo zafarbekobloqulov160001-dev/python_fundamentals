939f856fabf2ece5413a4ed83ca32f57ae208352
Zafarbek Obloqulov <zafarbekobloqulov160001@gmail.com>
2026-03-01 15:53:52 +0500

    Delete password_checker.py
    u98o6fkf7yk

--- password_checker.py
+++
@@ -1,8 +0,0 @@
-#This file check your password
-# This program checks whether a password is strong or weak.
-# if your password more than 9 character,your password is strong
-password=input("your password=")
-if len(password)>=9:
-    print("strong password")
-else :
-    print("weak password")    
