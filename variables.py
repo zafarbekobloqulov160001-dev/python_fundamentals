c9a61494112bdb6ca7ea7fcaebf70b3ec153b683
Zafarbek <zafarbekobloqulov160001@gmail.com>
2026-03-07 11:51:09 +0500

    Update variable file

--- variables.py
+++ variables.py
@@ -1,1 +1,8 @@
-print("hello  some github")
+# Variables example in Python
+
+name = "Zafar"             
+age = 20                 
+# name and age ------> variable name
+# "Zafarbek" and 20 -------- variable value
+print("My name is", name)
+print("I am", age, "years old")
